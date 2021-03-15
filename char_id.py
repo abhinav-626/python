c4e9afb804bf99fc1df60cc05afbf9cef5370113
abhinav-626 <80679609+abhinav-626@users.noreply.github.com>
2021-03-15 02:51:24 -0700

    second commit

---
+++ char_id.py
@@ -0,0 +1,9 @@
+a=input("enter the string:")
+if a.isupper():
+    print("The given string is in uppercase")
+elif a.islower():
+    print("The given string is in lowercase")
+elif a.islpha():
+    print("The given string is alphabet")
+else:
+    print("The given string is special character")
